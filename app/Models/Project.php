da672e5c81f2abc022ac22a72a65632d90871b7c
Mészáros Ádám <meszike91@gmail.com>
2022-10-25 12:14:13 +0200

    vue configuration

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use PhpParser\Node\Expr\AssignOp\Concat;
 
 class Project extends Model
 {
     use HasFactory;
+
+    public function contacts(){
+        return $this->hasMany(Concat::class);
+    }
 }
